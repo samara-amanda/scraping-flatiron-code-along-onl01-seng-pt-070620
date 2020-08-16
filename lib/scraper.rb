7c1d701eb35446e1f7b6378be12b11502d478771
Samara Grant <samara.amanda01@gmail.com>
2020-08-16 02:10:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+  end
+
+  def make_courses
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +26,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
